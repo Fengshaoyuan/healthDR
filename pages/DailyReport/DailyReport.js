8494343d7c82fbbace855421643b2b0b0f366042
hongfaqiu <951142905@qq.com>
2020-05-13 18:57:39 +0800

    问卷功能完善，二级问卷

--- pages/DailyReport/DailyReport.js
+++ pages/DailyReport/DailyReport.js
@@ -2,322 +2,364 @@
 var util = require('../../utils/util.js');
 
 Page({
+  // _handlerRadio: function () {
+    
+  // },
+  radioChange: function (e) {
+    let arr = e.detail.value.split(',')
+    console.log(arr[0])
+    let str = "message." + arr[0]
+    this.setData({
+      [str]: arr[1],
+      'questionnaire[10].visible': (this.data.message.sfyyjc == 0),
+      'questionnaire[11].visible': (this.data.message.sfyyjc == 0),
+      'questionnaire[12].visible': (this.data.message.sfyyjc == 0),
+    })
+    if(arr[0] == "sfzx") {
+      this.setData({
+      'questionnaire[1].visible': (this.data.message.sfzx == 0),
+      })
+    }
+    if(arr[0] == "tw" || arr[0] == "sfcxtz") {
+      this.setData({
+        'questionnaire[9].visible': (this.data.message.sfcxtz == 0 || this.data.message.tw == 0),
+      })
+    }
+    if(arr[0] == "sfyyjc") {
+      this.setData({
+        'questionnaire[10].visible': (this.data.message.sfyyjc == 0),
+        'questionnaire[11].visible': (this.data.message.sfyyjc == 0),
+        'questionnaire[12].visible': (this.data.message.sfyyjc == 0),
+      })
+    }
+    if(arr[0] == "sfjcbh") {
+      this.setData({
+      'questionnaire[14].visible': (this.data.message.sfjcbh == 0),
+      'questionnaire[17].visible': (this.data.message.sfjcbh == 0 || this.data.message.sfjcqz == 0),
+      'questionnaire[18].visible': (this.data.message.sfjcbh == 0 || this.data.message.sfjcqz == 0),
 
+      })
+    }
+    if(arr[0] == "sfjcqz") {
+      this.setData({
+      'questionnaire[16].visible': (this.data.message.sfjcqz == 0),
+      'questionnaire[17].visible': (this.data.message.sfjcbh == 0 || this.data.message.sfjcqz == 0),
+      'questionnaire[18].visible': (this.data.message.sfjcbh == 0 || this.data.message.sfjcqz == 0),
+      })
+    }
+    if(arr[0] == "sfcyglq") {
+      this.setData({
+      'questionnaire[19].visible': (this.data.message.sfcyglq == 0),
+      })
+    }
+    if(arr[0] == "sfcxzysx") {
+      this.setData({
+      'questionnaire[24].visible': (this.data.message.sfcxzysx == 0),
+      })
+    }
+    if(arr[0] == "sfsqhzjkk") {
+      this.setData({
+      'questionnaire[26].visible': (this.data.message.sfsqhzjkk == 0),
+      })
+    }
+    if(arr[0] == "sfymqjczrj") {
+      this.setData({
+      'questionnaire[28].visible': (this.data.message.sfymqjczrj == 0),
+      'questionnaire[29].visible': (this.data.message.sfymqjczrj == 0),
+      })
+    }
+    if(arr[0] == "sfyrjjh") {
+      this.setData({
+      'questionnaire[30].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[31].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[32].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[33].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[34].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[35].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[36].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[37].visible': (this.data.message.sfyrjjh == 0),
+      'questionnaire[38].visible': (this.data.message.sfyrjjh == 0),
+      })
+    }
+  },
   /**
    * 页面的初始数据
    */
   data: {
     canSubmit: false,
-    date: "",
-    realName: "张三",
-    studentID: "3170100001",
-    location: "",
-    questionare: [
+    message: {
+      date: "",
+      realName: "张三",
+      studentID: "3170100001",
+      location: "",
+      sfzx:0,
+      sfcxtz:1,
+      sfzgn: "",
+      szdd: "",
+      sffrqjwdg: 0,
+      sfqtyyqjwdg: "",
+      tw: 1,
+      sfcxtz: 1,
+      sfyyjc: 1,
+      sfjcbh: 1,
+      sfjcqz: 1,
+      jrsfqzys: 1,
+      jrdqtlqk: 8,
+      sfhsjc: 1,
+      sfcxzysx: 1,
+      sfsqhzjkk: 1,
+      sfymqjczrj: 1
+    },
+    questionnaire: [
       {
-      type: 1,
-        name: "sfzx",
-        hidden: false,
-        desc: "今日是否在校？ Are you on campus today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
-      },{
-        type: 2,
-        name: "fxyy",
-        hidden: true,
-        desc: "返校原因 Reason for returning to school",
-        answer: ""
-      }
-    ],
-    questionItems: [
-      {
+        id: 0,
         type: 1,
         name: "sfzx",
-        hidden: false,
+        visible: true,
         desc: "今日是否在校？ Are you on campus today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No"]
       },{
+        id: 1,
         type: 2,
         name: "fxyy",
-        hidden: true,
+        visible: false,
         desc: "返校原因 Reason for returning to school",
         answer: ""
       },{
+        id: 2,
         type: 1,
         name: "sfzgn",
-        hidden: false,
+        visible: true,
         desc: "所在地点 Your Location",
-        option: [
-          { id: 1, value: "境内" },
-          { id: 2, value: "境外" }
-        ]
+        option: ["境内","境外"]
       },{
+        id: 3,
         type: 3,
         name: "szdd",
-        hidden: false,
+        visible: true,
         desc: "所在地点（请打开手机位置功能，并在手机权限设置中选择允许访问位置信息）Your location (Please turn on the location access function on your mobile phone and allow App to access your location)"
       },{
+        id: 4,
         type: 1,
         name: "bztcyy",
-        hidden: true,
+        visible: false,
         desc: "当前地点与上次不在同一城市，原因如下 Current location is different from last time，why",
-        option: [
-          { id: 1, value: "探亲" },
-          { id: 2, value: "旅游" },
-          { id: 3, value: "其他" }
-        ]
+        option: ["探亲" ,"旅游" ,"其他" ]
       },{
+        id: 5,
         type: 1,
         name: "sffrqjwdg",
-        hidden: false,
+        visible: true,
         desc: "今日是否因发热请假未到岗（教职工）或未返校（学生）？ Are you on leave due to fever?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 6,
         type: 1,
         name: "sfqtyyqjwdg",
-        hidden: false,
+        visible: true,
         desc: "今日是否因发热外的其他原因请假未到岗（教职工）或未返校（学生）？ Are you on leave due to other symptoms except fever?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 7,
         type: 1,
         name: "tw",
-        hidden: false,
+        visible: true,
         desc: "今日是否有发热症状（高于37.2 ℃）？ Do you have a fever(above 37.2℃) today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 8,
         type: 1,
         name: "sfcxtz",
-        hidden: false,
+        visible: true,
         desc: "今日是否有咳嗽、呼吸道不畅、腹泻等其他症状？ Do you have cough, poor respiratory tract, diarrhea and other symptoms today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 9,
         type: 1,
         name: "sfyyjc",
-        hidden: true,
+        visible: false,
         desc: "是否到相关医院或门诊检查？ Have you been to hospital or clinic today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 10,
         type: 1,
         name: "jcjgqr",
-        hidden: true,
+        visible: false,
         desc: "检查结果属于以下哪种情况？ Choose your test result",
-        option: [
-          { id: 1, value: "疑似感染 With suspected Novel coronavirus pneumonia infection" },
-          { id: 2, value: "确诊感染 Confirmed Novel coronavirus pneumonia infection" },
-          { id: 3, value: "其他 Other" }
-        ]
+        option: ["疑似感染 With suspected Novel coronavirus pneumonia infection" ,"确诊感染 Confirmed Novel coronavirus pneumonia infection" ,"其他 Other" ]
       },{
+        id: 11,
         type: 2,
         naem: "gcjg",
-        hidden: true,
+        visible: false,
         desc: "观察或诊疗情况 Doctor's diagnosis",
         answer: ""
       },{
+        id: 12,
         type: 2,
         naem: "jcjg",
-        hidden: true,
+        visible: false,
         desc: "检查结果 Diagnose result",
         answer: ""
       },{
+        id: 13,
         type: 1,
         name: "sfjcbh",
-        hidden: false,
+        visible: true,
         desc: "今日是否接触过新冠肺炎疑似感染者？ Have you met suspected Novel coronavirus pneumonia patient today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
-        type: 1,
+        id: 14,
+        type: 2,
         name: "jcbhrq",
-        hidden: true,
+        visible: false,
         desc: "接触疑似人群时间 When did you met the suspected patient today?",
         answer: ""
       },{
+        id: 15,
         type: 1,
         name: "sfjcqz",
-        hidden: false,
+        visible: true,
         desc: "今日是否接触过新冠肺炎感染者？ Have you met confirmed Novel coronavirus pneumonia patient today",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 16,
         type: 1,
         name: "jcqzrq",
-        hidden: true,
+        visible: false,
         desc: "接触确诊人群时间 When did you met the Novel coronavirus pneumonia patient today?",
         answer: ""
       },{
+        id: 17,
         type: 1,
         name: "sfyqjzgc",
-        hidden: true,
+        visible: false,
         desc: "今日是否被当地管理部门要求在集中隔离点医学观察？ Have you been isolated by the local authorities at centralized isolation point for medical observation today?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 18,
         type: 1,
         name: "sfcyglq",
-        hidden: true,
+        visible: false,
         desc: "今日是否居家隔离观察（居家非隔离状态填否）? Have you been in self-isolation at home today (select No if you are just stay at home but not isolated)?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 19,
         type: 1,
         name: "sfjcqz",
-        hidden: true,
+        visible: false,
         desc: "观察开始时间 The start time",
         answer: ""
       },{
+        id: 20,
         type: 1,
         name: "jrsfqzys",
-        hidden: false,
+        visible: true,
         desc: "今日是否确诊疑似新冠肺炎？ Have you been diagnosed as suspected Novel coronavirus pneumonia patient?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 21,
         type: 1,
         name: "jrdqtlqk",
-        hidden: false,
+        visible: true,
         desc: "你是否4月10日后从下列地区返回浙江（含经停）? Did you return to Zhejiang from the following areas after April 10th (including stopovers)？",
-        option: [
-          { id: 1, value: "武汉 Wuhan" },
-          { id: 2, value: "湖北（除武汉） Hubei (non-Wuhan regions)" },
-          { id: 3, value: "哈尔滨市 Harbin" },
-          { id: 4, value: "绥芬河市 Suifenhe" },
-          { id: 5, value: "满洲里市 Manzhouli" },
-          { id: 6, value: "广州市 Guangzhou" },
-          { id: 7, value: "深圳市 Shenzhen" },
-          { id: 8, value: "揭阳市 Jieyang" },
-          { id: 9, value: "否 None of the above" }
-        ]
+        option: ["武汉 Wuhan" ,"湖北（除武汉） Hubei (non-Wuhan regions)" ,"哈尔滨市 Harbin" ,"绥芬河市 Suifenhe" ,"满洲里市 Manzhouli" ,"广州市 Guangzhou" ,"深圳市 Shenzhen" ,"揭阳市 Jieyang" ,"否 None of the above" ]
       },{
+        id: 22,
         type: 1,
         name: "sfhsjc",
-        hidden: false,
+        visible: true,
         desc: "你是否做过核酸检测？Did you screen by COVID-2019 Nucleic Acid Diagnosis Kit?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 23,
         type: 1,
         name: "sfcxzysx",
-        hidden: false,
+        visible: true,
         desc: "是否有任何与疫情相关的，值得注意的情况？ Do you have any situation related to the epidemic that deserves attention?",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 24,
         type: 2,
         name: "qksm",
-        hidden: true,
+        visible: false,
         desc: "情况说明 Situation explanation",
         answer: ""
       },{
+        id: 25,
         type: 1,
         name: "sfsqhzjkk",
-        hidden: false,
+        visible: true,
         desc: "是否已经申领校区所在地健康码？Have you got the health code of the city where the campus is located？",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes" ,"否 No" ]
       },{
+        id: 26,
         type: 1,
         name: "sqhzjkkys",
-        hidden: true,
+        visible: false,
         desc: "今日申领校区所在地健康码的颜色？What's the color of today's health code？",
-        option: [
-          { id: 1, value: "绿码" },
-          { id: 2, value: "黄码" },
-          { id: 3, value: "橙码" },
-          { id: 4, value: "红码" }
-        ]
+        option: ["绿码" ,"黄码" ,"橙码" ,"红码" ]
       },{
+        id: 27,
         type: 1,
         name: "sfymqjczrj",
-        hidden: false,
+        visible: true,
         desc: "本人家庭成员(包括其他密切接触人员)是否有近14日入境或近14日拟入境的情况？Have your family members (including other close contact persons）entered Chinese Mainland over the past 14 days or plan to enter Chinese Mainland in 14 days?",
-        option: [
-          { id: 1, value: "是 Yes，请及时向所在单位报告实际情况。please contact your college/school immediately"},
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes，请及时向所在单位报告实际情况。please contact your college/school immediately","否 No" ]
       },{
+        id: 28,
         type: 2,
         name: "zjdfgj",
-        hidden: true,
+        visible: false,
         desc: "近14日到访过的国家/地区 Countries you’ve visited over the past 14 days",
         answer: ""
       },{
+        id: 29,
         type: 1,
         name: "sfyrjjh",
-        hidden: true,
+        visible: false,
         desc: "未来14天内是否有入境计划? Do you have plan to enter Chinese Mainland in the next 14 days?",
-        option: [
-          { id: 1, value: "是 Yes"},
-          { id: 2, value: "否 No" }
-        ]
+        option: ["是 Yes","否 No" ]
       },{
+        id: 30,
         type: 2,
         name: "cfgj",
-        hidden: true,
+        visible: false,
         desc: "出发国家或地区 Departure country or region?",
         answer: ""
       },{
+        id: 31,
         type: 2,
         name: "tjgj",
-        hidden: true,
+        visible: false,
         desc: "途经国家/地区（直航填无）Transit Country/region? (Enter “no” if direct flights).",
         answer: ""
       },{
+        id: 32,
         type: 2,
         name: "nrjrq",
-        hidden: true,
+        visible: false,
         desc: "拟入境日期（北京时间） Planned date of arrival",
         answer: ""
       },{
+        id: 33,
         type: 2,
         name: "rjka",
-        hidden: true,
+        visible: false,
         desc: "入境口岸 Port of Entry",
         answer: ""
       },{
+        id: 34,
         type: 2,
         name: "jnmdd",
-        hidden: true,
+        visible: false,
         desc: "境内目的地 Destination in Chinese Mainland",
         answer: ""
       },{
+        id: 35,
         type: 1,
         name: "rjjtfs",
-        hidden: true,
+        visible: false,
         desc: "入境交通方式 Mode of transport by which you enter Chinese Mainland",
         option: [
               { id: 1, type: "飞机 By plane" },
@@ -325,15 +367,17 @@ Page({
               { id: 3, type: "其他 Other" }
             ]
       },{
+        id: 36,
         type: 2,
         name: "rjjtgjbc",
-        hidden: true,
+        visible: false,
         desc: "入境交通工具班次（航班号，火车车次） Details of the inbound transportation (Flight number, train number)",
         answer: ""
       },{
+        id: 37,
         type: 1,
         name: "jnjtfs",
-        hidden: true,
+        visible: false,
         desc: "境内交通方式 Mode of transport in Chinese Mainland",
         option: [
               { id: 1, type: "飞机 By plane" },
@@ -341,26 +385,17 @@ Page({
               { id: 3, type: "其他 Other" }
             ]
       },{
-        type: 1,
-        name: "sfsqhzjkk",
-        hidden: false,
-        desc: "是否已经申领校区所在地健康码？Have you got the health code of the city where the campus is located？",
-        option: [
-          { id: 1, value: "是 Yes" },
-          { id: 2, value: "否 No" }
-        ]
-      },{
+        id: 38,
         type: 2,
         name: "jnjtgjbc",
-        hidden: true,
+        visible: false,
         desc: "境内交通工具班次（航班号，火车车次，汽车牌照） Transportation details in Chinese Mainland (Flight number, train number, license plate)",
         answer: ""
       },{
+        id: 39,
         type: 1,
-        hidden: false,
-        option: [
-          { id: 1, value: "本人承诺：\n上述信息真实准确。如有变化，及时更新相关信息并报告所在单位。\n本人已知晓并将遵守政府和学校相关规定，配合做好疫情防控工作。\nI agree:\nThe above information is true and accurate. In case of changes, I will keep my information updated and report in a timely manner.\nI have understood and will abide by the relevant government and University regulations to facilitate the prevention and control of COVID-19 epidemic." }
-        ]
+        visible: true,
+        option: ["本人承诺：\n上述信息真实准确。如有变化，及时更新相关信息并报告所在单位。\n本人已知晓并将遵守政府和学校相关规定，配合做好疫情防控工作。\nI agree:\nThe above information is true and accurate. In case of changes, I will keep my information updated and report in a timely manner.\nI have understood and will abide by the relevant government and University regulations to facilitate the prevention and control of COVID-19 epidemic." ]
       }
     ]
   },
@@ -371,8 +406,9 @@ Page({
   onLoad: function (options) {
     var DATE = util.formatDate(new Date())
     this.setData({
-    date: DATE,
+    'message.date': DATE
     })
+
   },
 
   /**
@@ -386,7 +422,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.setData({
+      
+    })
   },
 
   /**
